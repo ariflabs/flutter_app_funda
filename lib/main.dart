d9ae7a1ecd1cd5fec669b3beeaeccd581bef7737
Arif Hidayat <arifaalfatih@gmail.com>
2024-11-11 08:34:13 +0700

    widget/appbarr

--- lib/main.dart
+++ lib/main.dart
@@ -14,23 +14,45 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-          backgroundColor: Colors.deepPurple[300],
-          body: Center(
-            child: Container(
-              height: 300,
-              width: 300,
-              decoration: BoxDecoration(
-                color: Colors.deepPurple[500],
-                borderRadius: BorderRadius.circular(20),
-              ),
-              padding: EdgeInsets.all(25),
-              child: Icon(
-                Icons.home,
-                size: 100,
+        backgroundColor: Colors.deepPurple[200],
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "My App Bar Funda",
+              style: TextStyle(
                 color: Colors.white,
+                fontWeight: FontWeight.bold,
               ),
             ),
-          )),
+          ),
+          backgroundColor: Colors.deepPurple,
+          elevation: 0,
+          leading: Icon(Icons.menu, color: Colors.white),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(Icons.logout),
+              color: Colors.white,
+            ),
+          ],
+        ),
+        body: Center(
+          child: Container(
+            height: 300,
+            width: 300,
+            decoration: BoxDecoration(
+              color: Colors.deepPurple[500],
+              borderRadius: BorderRadius.circular(20),
+            ),
+            padding: EdgeInsets.all(25),
+            child: Icon(
+              Icons.home,
+              size: 100,
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
