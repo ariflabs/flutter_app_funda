a552149fd7723b671d9e27de241eac38b11b0fd9
Arif Hidayat <arifaalfatih@gmail.com>
2024-11-16 17:39:36 +0700

    widget listview builder

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,28 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  List names = ["Arif Hidayat", "Acep Cepot", "Pinokio"];
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        body: ListView(
-          scrollDirection: Axis.horizontal,
-          children: [
-            //1st box
-            Container(
-              width: 300,
-              // height: 200,
-              color: Colors.deepPurple,
-            ),
-
-            //2nd box
-            Container(
-              width: 300,
-              // height: 200,
-              color: Colors.deepPurple[300],
-            ),
-
-            //3rd box
-            Container(
-              width: 300,
-              // height: 200,
-              color: Colors.deepPurple[200],
-            ),
-          ],
+        body: ListView.builder(
+          itemCount: names.length,
+          // itemCount: 10,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(names[index]),
+            // title: Text(index.toString()),
+          ),
         ),
       ),
     );
