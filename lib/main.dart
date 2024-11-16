75ce1ff3292546d3f214f921eb4efa6628a992c2
Arif Hidayat <arifaalfatih@gmail.com>
2024-11-16 17:23:10 +0700

    widget ListView

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,35 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        body: ListView(
+          scrollDirection: Axis.horizontal,
+          children: [
+            //1st box
+            Container(
+              width: 300,
+              // height: 200,
+              color: Colors.deepPurple,
+            ),
+
+            //2nd box
+            Container(
+              width: 300,
+              // height: 200,
+              color: Colors.deepPurple[300],
+            ),
+
+            //3rd box
+            Container(
+              width: 300,
+              // height: 200,
+              color: Colors.deepPurple[200],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
