60c4f76232887b11b5ec37cd086c51771eea6ef7
Arif Hidayat <arifaalfatih@gmail.com>
2024-11-17 21:52:47 +0700

    widget gesture tap

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,39 @@
+// ignore_for_file: prefer_const_constructors_in_immutables, prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  void onTapped() {
+    print("User Tapped the Container");
+  }
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(),
+      home: Scaffold(
+        body: Center(
+          child: GestureDetector(
+            onTap: onTapped,
+            // onTap: () {
+            //   // do somthing when user tapped the container.
+            //   print("User Tapped the Container");
+            // },
+            child: Container(
+              height: 200,
+              width: 200,
+              color: Colors.deepPurple[200],
+              child: Center(child: Text("Tap Me!")),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
